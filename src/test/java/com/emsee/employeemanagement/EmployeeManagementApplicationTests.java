7e6f58dfcfc266af43a9977fffd7594f29f99e48
SrilekhaShankar22 <118003240+SrilekhaShankar22@users.noreply.github.com>
2026-08-10 01:21:10 +0530

    on Main branch, build failes #1, commented

--- src/test/java/com/emsee/employeemanagement/EmployeeManagementApplicationTests.java
+++ src/test/java/com/emsee/employeemanagement/EmployeeManagementApplicationTests.java
@@ -2,8 +2,10 @@ package com.emsee.employeemanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmployeeManagementApplicationTests {
 
 	@Test
